6d084d3dc7ac83d3fce362ce0001beed26d30631
busisiwe <busisiwedladla283@gmail.com>
2026-06-26 14:06:14 +0200

    new changes

--- src/Deposit.java
+++ src/Deposit.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
+mport java.util.Scanner;
 
 public class Deposit {
 
   public void depositMoney(Balance account) {
-
     Scanner input = new Scanner(System.in);
 
     System.out.println("Enter deposit amount:");
     double amount = input.nextDouble();
 
-    if (amount > 0) {
+    if (amount >= 10 && amount % 10 == 0) {
       account.balance += amount;
       System.out.println("Deposit successful!");
+      System.out.println("New balance: R" + account.balance);
+
+    } else if (amount < 10) {
+      System.out.println("Minimum deposit is R10!");
+
     } else {
-      System.out.println("Invalid amount!");
+      System.out.println("Deposit must be multiples of R10!");
     }
   }
-
-
 }
--- src/Withdraw.java
+++ src/Withdraw.java
@@ -3,18 +3,22 @@ import java.util.Scanner;
 public class Withdraw {
 
   public void withdrawMoney(Balance account) {
-    System.out.println("Withdrawing from: " + account.balance);
+    System.out.println("Withdrawing from: R" + account.balance);
 
     Scanner input = new Scanner(System.in);
 
     System.out.println("Enter withdrawal amount:");
     double amount = input.nextDouble();
 
-    if (amount > 0 && amount <= account.balance) {
+    if (amount >= 10 && amount % 10 == 0 && amount <= account.balance) {
       account.balance -= amount;
       System.out.println("Withdrawal successful!");
+
+    } else if (amount < 10) {
+      System.out.println("Minimum withdrawal is R10!");
+
     } else {
-      System.out.println("insufficient balance!");
+      System.out.println("Insufficient balance!");
     }
   }
 }
